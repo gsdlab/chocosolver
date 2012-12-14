1181a6b7efebcedba095505cb65705e97416c65d
Jimmy Liang <jliang@gsd.uwaterloo.ca>
2012-12-14 10:40:23 -0500

    Implemented more constraints.

--- src/main/java/org/clafer/ChocoSolver.java
+++ src/main/java/org/clafer/ChocoSolver.java
@@ -1,14 +1,15 @@
 package org.clafer;
 
+import choco.cp.solver.search.integer.valiterator.IncreasingDomain;
+import choco.cp.solver.search.integer.varselector.MostConstrained;
+import choco.cp.solver.search.set.MinDomSet;
+import choco.Options;
 import choco.cp.solver.search.integer.branching.AssignVar;
 import javax.script.ScriptEngine;
 import choco.kernel.solver.Solver;
 import choco.cp.model.CPModel;
 import choco.cp.solver.CPSolver;
-import choco.cp.solver.search.integer.valiterator.IncreasingDomain;
 import choco.cp.solver.search.set.AssignSetVar;
-import choco.cp.solver.search.set.MinDomSet;
-import choco.cp.solver.search.set.MinEnv;
 import choco.kernel.model.Model;
 import choco.kernel.model.variables.set.SetVariable;
 import java.io.File;
@@ -18,9 +19,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import javax.script.ScriptEngineManager;
 import static choco.Choco.*;
-import choco.cp.solver.search.integer.varselector.MostConstrained;
-import choco.kernel.common.logging.ChocoLogging;
+import choco.cp.solver.search.set.MinEnv;
+import choco.kernel.model.variables.integer.IntegerVariable;
+import choco.kernel.solver.variables.integer.IntDomainVar;
+import choco.kernel.solver.variables.set.SetVar;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import javax.script.Invocable;
 import javax.script.ScriptException;
 
@@ -35,7 +41,7 @@ public class ChocoSolver {
             if (args.length == 0) {
                 System.out.println("Expected at least one argument.");
             }
-            if(args.length == 1) {
+            if (args.length == 1) {
                 run(false, new File(args[0]));
             } else {
                 run(Boolean.parseBoolean(args[0]), new File(args[1]));
@@ -46,10 +52,11 @@ public class ChocoSolver {
     }
 
     public static void run(boolean all, File in) throws IOException, ScriptException, NoSuchMethodException {
-
         Model m = new CPModel();
-        Solver s = new CPSolver();
-        
+        m.setDefaultExpressionDecomposition(false);
+
+        CPSolver s = new CPSolver();
+
         SetVariable root = constant(new int[]{0});
         m.addConstraint(eqCard(root, 1));
         SetVariable[] __root = new SetVariable[]{root};
@@ -75,13 +82,12 @@ public class ChocoSolver {
         engine.put(ScriptEngine.FILENAME, in.getName());
         engine.eval(script);
 
-//        ChocoLogging.toVerbose();
-//        ChocoLogging.toSearch();
+//        ChocoLogging.setVerbosity(Verbosity.FINEST);
         s.read(m);
-
+        
         s.addGoal(new AssignSetVar(new MinDomSet(s), new MinEnv()));
         s.addGoal(new AssignVar(new MostConstrained(s), new IncreasingDomain()));
-
+        
         System.out.println("Solving");
         if (!Boolean.TRUE.equals(s.solve())) {
             System.err.println("No solution.");
@@ -93,6 +99,43 @@ public class ChocoSolver {
 
         while (all && (System.in.read()) != -1 && s.nextSolution()) {
             i.invokeFunction("solution", s);
+        }
+    }
+
+    public static SetVar[] getDecisionSetVars(Model m, Solver s) {
+        List<SetVar> svs = new ArrayList<SetVar>();
+        Iterator<SetVariable> st = m.getSetVarIterator();
+        while (st.hasNext()) {
+            SetVariable sv = st.next();
+            if (!sv.getOptions().contains(Options.V_NO_DECISION)) {
+                svs.add(s.getVar(sv));
+            }
+        }
+        return svs.toArray(new SetVar[svs.size()]);
+    }
+
+    public static IntDomainVar[] getDecisionIntVariables(Model m, Solver s) {
+        List<IntDomainVar> ivs = new ArrayList<IntDomainVar>();
+        Iterator<IntegerVariable> it = m.getIntVarIterator();
+        while (it.hasNext()) {
+            IntegerVariable iv = it.next();
+            if (!iv.getOptions().contains(Options.V_NO_DECISION)) {
+                ivs.add(s.getVar(iv));
+            } else {
+                System.out.println("NOP:" + iv);
+            }
+        }
+        return ivs.toArray(new IntDomainVar[ivs.size()]);
+    }
+
+    public static void printAllVariables(Model m) {
+        Iterator<IntegerVariable> it = m.getIntVarIterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+        Iterator<SetVariable> st = m.getSetVarIterator();
+        while (st.hasNext()) {
+            System.out.println(st.next());
         }
     }
 
--- src/main/resources/org/clafer/header.js
+++ src/main/resources/org/clafer/header.js
@@ -1,18 +1,18 @@
 importClass(Packages.choco.Choco);
 importClass(Packages.choco.Options);
-importClass(Packages.choco.kernel.model.variables.ConstantFactory);
-importClass(Packages.org.clafer.Util);
-importClass(java.util.Arrays);
 
 function alert(msg) {
     throw new Error(msg);
 }
 
-function set (name, low, high) {
+function set (name, low, high, decision) {
     if (typeof name === "undefined") alert("set: name is undefined");
     if (typeof low === "undefined") alert("set: low is undefined");
     if (typeof high === "undefined") alert("set: high is undefined");
 
+    if(decision) {
+        return Choco.makeSetVar(name, low, high, [Options.V_ENUM, Options.V_NO_DECISION]);
+    }
     return Choco.makeSetVar(name, low, high, [Options.V_ENUM]);
 }
 
@@ -25,22 +25,27 @@ function setArray (name, dimension, low, high) {
     return Choco.makeSetVarArray(name, dimension, low, high, [Options.V_ENUM]);
 }
 
-function intArray (name, dimension, low, high) {
+function intArray (name, dimension, low, high, decision) {
     if (typeof name === "undefined") alert("intArray: name is undefined");
     if (typeof dimension === "undefined") alert("intArray: dimension is undefined");
 
-    if (typeof low === "undefined" && typeof high === "undefined") {
-        return Choco.makeIntVarArray(name, dimension, []);    
-    }
+    if (typeof low === "undefined") alert("intArray: low is undefined");
+    if (typeof high === "undefined") alert("intArray: high is undefined");
 
+    if (decision) {
+        return Choco.makeIntVarArray(name, dimension, low, high, [Options.V_ENUM, Options.V_NO_DECISION]);
+    }
     return Choco.makeIntVarArray(name, dimension, low, high, [Options.V_ENUM]);
 }
 
-function int (name, low, high) {
+function int (name, low, high, decision) {
     if (typeof name === "undefined") alert("int: name is undefined");
     if (typeof low === "undefined") alert("int: low is undefined");
     if (typeof high === "undefined") alert("int: high is undefined");
 
+    if (decision) {
+        return Choco.makeIntVar(name, low, high, [Options.V_ENUM, Options.V_NO_DECISION]);
+    }
     return Choco.makeIntVar(name, low, high, [Options.V_ENUM]);
 }
 
@@ -60,6 +65,9 @@ function between (low, int, high) {
 }
 var neq = Choco.neq;
 var and = Choco["and(choco.kernel.model.constraints.Constraint[])"];
+var or = Choco["or(choco.kernel.model.constraints.Constraint[])"];
+var plus = Choco.plus;
+var minus = Choco.minus;
 var sum = Choco.sum;
 
 var joinChildCount = 0;
@@ -105,6 +113,25 @@ function joinParent (expr, scope, parent, parentScope) {
 
 var joinRefCount = 0;
 
+function joinRefFixed (expr, scope, ref, refLow, refHigh, card) {
+    joinRefCount++;
+    var elems = int("@__joinRefFixedElems" + joinRefCount, card, 0, scope - 1);
+    var refd  = int("@__joinRefFixedRefd" + joinRefCount, card, refLow, refHigh);
+    var result = set("@__joinRefFixed" + joinRefCount, refLow, refHigh);
+    
+    addConstraint(member(expr, elem));
+    //addConstraint(allDifferent(elems));
+    
+    //for(var i = 0; i < card; i++) {
+    addConstraint(nth(elem, ref, refd));
+    //}
+    //addConstraint(member(result, refd));
+    //addConstraint(eqCard(result, 1));
+
+    //return result;
+    return refd;
+}
+
 // Optimized for references with VERY large domains (ie. integers).
 function joinRef (expr, scope, ref, refLow, refHigh) {
     var refSets = []
@@ -122,6 +149,78 @@ function joinRef (expr, scope, ref, refLow, refHigh) {
 
     addConstraint(setUnion(refSets, s));
     return s;
+}
+
+var varCounter = 0;
+function name(n) {
+    varCounter++;
+    return n + "#" + varCounter;
+}
+
+function enumerate(list, num) {
+    if (num <= 0) alert("enumerate: num <= 0");
+    if (num == 1) return list.map(function(i) { return [i]; });
+    
+    var enums = [];
+    var recenums = enumerate(list, num - 1);
+    for (var l in list) {
+        for (var k in recenums) {
+            enums.push(recenums[k].concat([list[l]]));
+        }
+    }
+    return enums;
+}
+
+function enumerateDisj(list, num) {
+    if (num <= 0) alert("enumerate: num <= 0");
+    if (num > list.length) return [];
+    if (num == 1) return list.map(function(i) { return [i]; });
+    
+    var enums = [];
+    for (var l = 0; l < list.length; l++) {
+        var recenums = enumerateDisj(list.slice(l + 1), num - 1);
+        for (var k in recenums) {
+            enums.push([list[l]].concat(recenums[k]));
+        }
+    }
+    return enums;
+}
+
+// Helper function, do not call outside of this library.
+function quantify(pred, quant, sizeLow, sizeHigh, low, high, bodyFun, disj, numVars) {
+    var elems = intArray(name("allQuant"), sizeHigh, low, high);
+    
+    var tests = [];
+
+    for(var i = sizeLow; i <= sizeHigh; i++) {
+        // If i == 0 then the quantifier is satisfied.
+        if (i > 0) {
+            var subelems = elems.slice(0, i);
+
+            var enums;
+            if (disj) {
+                enums = enumerateDisj(subelems, numVars);
+            } else {
+                enums = enumerate(subelems, numVars);
+            }
+
+            if(enums.length > 0) {
+                tests.push(implies(eqCard(quant, i),
+                        and(
+                            [allDifferents(subelems), members(quant, subelems), pred(enums.map(bodyFun))])
+                        ));
+            }
+        }
+    }
+    return and(tests);
+}
+
+function allQuant(quant, sizeLow, sizeHigh, low, high, bodyFun, disj, numVars) {
+    return quantify(and, quant, sizeLow, sizeHigh, low, high, bodyFun, disj, numVars);
+}
+
+function someQuant(quant, sizeLow, sizeHigh, low, high, bodyFun, disj, numVars) {
+    return quantify(or, quant, sizeLow, sizeHigh, low, high, bodyFun, disj, numVars);
 }
 
 var emptySet = Choco.emptySet();
@@ -165,6 +264,10 @@ function ifThenElse(a, b, c) {
     return and([implies(a, b), implies(not(a), c)]);
 }
 var ifOnlyIf = Choco.ifOnlyIf;
+// Due to bug, this less than optimal function is needed :(
+function members(s, is) {
+    return and(is.map(function(i) {return member(s, i);}));
+}
 var member = Choco.member;
 var notMember = Choco.notMember;
 var nth = Choco.nth;
@@ -180,6 +283,16 @@ function betweenCard (low, set, high) {
         return leqCard(set, high);
     }
     return and([geqCard(set, low), leqCard(set, high)]);
+}
+// Due to bug
+function allDifferents(is) {
+    var tests = []
+    for(var i = 0; i < is.length; i++) {
+        for (var j = i + 1; j < is.length; j++) {
+            tests.push(neq(is[i], is[j]));
+        }
+    }
+    return and(tests);
 }
 var allDifferent = Choco.allDifferent
 var inverseSet = Choco.inverseSet
@@ -201,8 +314,6 @@ function getVar(v) {
     return s.getVar(v);
 }
 
-var combine = Util.combine
-
 var reify__counter = 0;
 
 function reifyConstraint(constraint) {
@@ -218,11 +329,15 @@ function uniqueRef(parent, parentScope, ref) {
     for(var diff1 = 0; diff1 < scope; diff1++) {
         var refdiff = [];
         for(var diff2 = diff1 + 1; diff2 < scope; diff2++) {
-            refdiff.push(implies(eq(parent[diff1], parent[diff2]), neq(ref[diff1], ref[diff2])));
+            var test = and([eq(parent[diff1], __this), eq(parent[diff2], __this)])
+            refdiff.push(implies(test, neq(ref[diff1], ref[diff2])));
         }
         if(refdiff.length > 0) {
-            fulldiff.push(implies(neq(parent[diff1], parentScope), and(refdiff)));
+            fulldiff = fulldiff.concat(refdiff);
         }
+    }
+    if(fulldiff.length == 1) {
+        return fulldiff[0];
     }
     return and(fulldiff);
 }
@@ -251,6 +366,57 @@ function offset(base, off) {
 
 var constant = Choco.constant;
 var constantInt = Choco["constant(int)"];
+var among = Choco.among;
+var increasingNValue = Choco.increasingNValue;
+
+var sumCount = 0;
+
+function sumSet(s, low, high) {
+    if (typeof s === "undefined") alert("sumSet: set is undefined");
+    if (typeof low === "undefined") alert("sumSet: low is undefined");
+    if (typeof high === "undefined") alert("sumSet: high is undefined");
+
+    var lowB = s.getLowB();
+    var uppB = s.getUppB();
+    var lowSumB = low == 0 ? lowB : (low / 2) * (lowB + lowB + low - 1);
+    var uppSumB = (high / 2) * (uppB - high + 1  + uppB);
+    
+    sumCount++;
+    var result = int("sumSet" + sumCount, lowSumB, uppSumB);
+    var elems  = intArray("sumSetElems" + sumCount, high, s.getLowB(), s.getUppB());
+
+    var imp = implies;
+    if (low == high) {
+        imp = constf;
+    }
+
+    if (high >= 2) {
+        addConstraint(allDifferent(elems));
+    }
+    for (var i = low; i <= high; i++) {
+        if (i == 0) {
+            addConstraint(imp(eqCard(s, 0), eq(result, 0)));
+        } else if(i == 1) {
+            addConstraint(imp(eqCard(s, 1), member(result, s)));
+        } else {
+            var subelems = elems.slice(0, i);
+            addConstraint(imp(eqCard(s, i), and([eq(result, sum(subelems))].concat(allMembers(subelems, s)))));
+        }
+    }
+    return result;
+}
+
+function constf(x, y) {
+    return y;
+}
+
+function allMembers(ivs, sv) {
+    memb = [];
+    for (var i in ivs) {
+        memb.push(member(ivs[i], sv));
+    }
+    return memb;
+}
 
 function range (low, high){
     var r = [];
@@ -265,20 +431,23 @@ var singletonCounter = 0;
 // TODO: deprecate when optimized fix-size sets are implemented
 function singleton(val) {
     singletonCounter++;
-    var sv = set(val.getName() + "__singleton" + singletonCounter, -10000, 1000);
+    var sv = set("__singleton__" + val.getName() + "_" + singletonCounter, val.getLowB(), val.getUppB(), true);
     addConstraint(eqCard(sv, 1));
     addConstraint(member(val, sv));
     return sv;
 }
 
-// http://stackoverflow.com/questions/1433382/how-to-convert-rhino-javascript-arrays-to-java-arrays
-function convertArray(type, arr) {
-    var jArr = java.lang.reflect.Array.newInstance(type, arr.length);
-    for (var i = 0; i < arr.length; i++) {
-        jArr[i] = arr[i];
-    }
-    return jArr;
-};
+function singletonExpr(expr) {
+    // TODO fix when bug patched.
+    singletonCounter++;
+    var singleSv = Choco.makeSetVar("__singletonExpr__" + expr.getName() + "_" + singletonCounter, expr.getLowB(), expr.getUppB(), [Options.V_ENUM]);
+    var singleIv = int("__singletonExpri__" + expr.getName() + "_" + singletonCounter, expr.getLowB(), expr.getUppB());
+
+    addConstraint(eq(singleIv, expr));
+    addConstraint(eqCard(singleSv, 1));
+    addConstraint(member(singleIv, singleSv));
+    return singleSv;
+}
 
 var TRUE = Choco.TRUE;
 var FALSE = Choco.FALSE;
