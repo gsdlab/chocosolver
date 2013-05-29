b65368d8ca01c02064557ff1dd3391318685aa8a
Jimmy Liang <paradoxical.reasoning@gmail.com>
2013-05-29 19:47:34 -0400

    Negate tests.

--- src/test/java/org/clafer/BoolArithmeticTest.java
+++ src/test/java/org/clafer/BoolArithmeticTest.java
@@ -52,6 +52,38 @@ public class BoolArithmeticTest {
      * A -> integer
      * B -> integer
      * C ?
+     * [some C => !(A.ref = 1 && B.ref = 2)]
+     * </pre>
+     */
+    @Test(timeout = 60000)
+    public void testNotAnd() {
+        /*
+         * import Control.Monad
+         *
+         * solutions = do
+         *     a <- [-2..2]
+         *     b <- [-2..2]
+         *     c <- [True, False]
+         *     when c $ guard $ not $ a == 1 && b == 2
+         *     return (a, b, c)
+         */
+        AstModel model = newModel();
+
+        AstConcreteClafer a = model.addChild("A").withCard(1, 1).refTo(IntType);
+        AstConcreteClafer b = model.addChild("B").withCard(1, 1).refTo(IntType);
+        AstConcreteClafer c = model.addChild("C").withCard(0, 1);
+        model.addConstraint(implies(some(c), not(and(
+                equal(joinRef(global(a)), constant(1)), equal(joinRef(global(b)), constant(2))))));
+
+        ClaferSolver solver = ClaferCompiler.compile(model, Scope.defaultScope(1).intLow(-2).intHigh(2));
+        assertEquals(49, solver.allInstances().length);
+    }
+
+    /**
+     * <pre>
+     * A -> integer
+     * B -> integer
+     * C ?
      * [some C => (A.ref = 1 &lt;=&gt; B.ref = 2)]
      * </pre>
      */
@@ -77,6 +109,38 @@ public class BoolArithmeticTest {
 
         ClaferSolver solver = ClaferCompiler.compile(model, Scope.defaultScope(1).intLow(-2).intHigh(2));
         assertEquals(42, solver.allInstances().length);
+    }
+
+    /**
+     * <pre>
+     * A -> integer
+     * B -> integer
+     * C ?
+     * [some C => !(A.ref = 1 &lt;=&gt; B.ref = 2)]
+     * </pre>
+     */
+    @Test(timeout = 60000)
+    public void testNotIfOnlyIf() {
+        /*
+         * import Control.Monad
+         *
+         * solutions = do
+         *     a <- [-2..2]
+         *     b <- [-2..2]
+         *     c <- [True, False]
+         *     when c $ guard $ not $ (a == 1) == (b == 2)
+         *     return (a, b, c)
+         */
+        AstModel model = newModel();
+
+        AstConcreteClafer a = model.addChild("A").withCard(1, 1).refTo(IntType);
+        AstConcreteClafer b = model.addChild("B").withCard(1, 1).refTo(IntType);
+        AstConcreteClafer c = model.addChild("C").withCard(0, 1);
+        model.addConstraint(implies(some(c), not(ifOnlyIf(
+                equal(joinRef(global(a)), constant(1)), equal(joinRef(global(b)), constant(2))))));
+
+        ClaferSolver solver = ClaferCompiler.compile(model, Scope.defaultScope(1).intLow(-2).intHigh(2));
+        assertEquals(33, solver.allInstances().length);
     }
 
     /**
@@ -119,6 +183,41 @@ public class BoolArithmeticTest {
      * A -> integer
      * B -> integer
      * C ?
+     * [some C => !(A.ref = 1 =&gt; B.ref = 2)]
+     * </pre>
+     */
+    @Test(timeout = 60000)
+    public void testNotImplies() {
+        /*
+         * import Control.Monad
+         *
+         * solutions = do
+         *     a <- [-2..2]
+         *     b <- [-2..2]
+         *     c <- [True, False]
+         *     when c $ guard $ not $ (a == 1) `implies` (b == 2)
+         *     return (a, b, c)
+         *     where
+         *         implies True False = False
+         *         implies _ _ = True
+         */
+        AstModel model = newModel();
+
+        AstConcreteClafer a = model.addChild("A").withCard(1, 1).refTo(IntType);
+        AstConcreteClafer b = model.addChild("B").withCard(1, 1).refTo(IntType);
+        AstConcreteClafer c = model.addChild("C").withCard(0, 1);
+        model.addConstraint(implies(some(c), not(implies(
+                equal(joinRef(global(a)), constant(1)), equal(joinRef(global(b)), constant(2))))));
+
+        ClaferSolver solver = ClaferCompiler.compile(model, Scope.defaultScope(1).intLow(-2).intHigh(2));
+        assertEquals(29, solver.allInstances().length);
+    }
+
+    /**
+     * <pre>
+     * A -> integer
+     * B -> integer
+     * C ?
      * [some C => (A.ref = 1 || B.ref = 2)]
      * </pre>
      */
@@ -144,6 +243,38 @@ public class BoolArithmeticTest {
 
         ClaferSolver solver = ClaferCompiler.compile(model, Scope.defaultScope(1).intLow(-2).intHigh(2));
         assertEquals(34, solver.allInstances().length);
+    }
+
+    /**
+     * <pre>
+     * A -> integer
+     * B -> integer
+     * C ?
+     * [some C => !(A.ref = 1 || B.ref = 2)]
+     * </pre>
+     */
+    @Test(timeout = 60000)
+    public void testNotOr() {
+        /*
+         * import Control.Monad
+         *
+         * solutions = do
+         *     a <- [-2..2]
+         *     b <- [-2..2]
+         *     c <- [True, False]
+         *     when c $ guard $ not $ (a == 1) || (b == 2)
+         *     return (a, b, c)
+         */
+        AstModel model = newModel();
+
+        AstConcreteClafer a = model.addChild("A").withCard(1, 1).refTo(IntType);
+        AstConcreteClafer b = model.addChild("B").withCard(1, 1).refTo(IntType);
+        AstConcreteClafer c = model.addChild("C").withCard(0, 1);
+        model.addConstraint(implies(some(c), not(or(
+                equal(joinRef(global(a)), constant(1)), equal(joinRef(global(b)), constant(2))))));
+
+        ClaferSolver solver = ClaferCompiler.compile(model, Scope.defaultScope(1).intLow(-2).intHigh(2));
+        assertEquals(41, solver.allInstances().length);
     }
 
     /**
@@ -183,6 +314,38 @@ public class BoolArithmeticTest {
      * A -> integer
      * B -> integer
      * C ?
+     * [some C => !(A.ref = 1 xor B.ref = 2)]
+     * </pre>
+     */
+    @Test(timeout = 60000)
+    public void testNotXor() {
+        /*
+         * import Control.Monad
+         *
+         * solutions = do
+         *     a <- [-2..2]
+         *     b <- [-2..2]
+         *     c <- [True, False]
+         *     when c $ guard $ not $ (a == 1) /= (b == 2)
+         *     return (a, b, c)
+         */
+        AstModel model = newModel();
+
+        AstConcreteClafer a = model.addChild("A").withCard(1, 1).refTo(IntType);
+        AstConcreteClafer b = model.addChild("B").withCard(1, 1).refTo(IntType);
+        AstConcreteClafer c = model.addChild("C").withCard(0, 1);
+        model.addConstraint(implies(some(c), not(xor(
+                equal(joinRef(global(a)), constant(1)), equal(joinRef(global(b)), constant(2))))));
+
+        ClaferSolver solver = ClaferCompiler.compile(model, Scope.defaultScope(1).intLow(-2).intHigh(2));
+        assertEquals(42, solver.allInstances().length);
+    }
+
+    /**
+     * <pre>
+     * A -> integer
+     * B -> integer
+     * C ?
      * [if some C then A.ref = 1 else B.ref = 2]
      * </pre>
      */
@@ -195,8 +358,7 @@ public class BoolArithmeticTest {
          *     a <- [-2..2]
          *     b <- [-2..2]
          *     c <- [True, False]
-         *     when c $ guard $ a == 1
-         *     when (not c) $ guard $ b == 1
+         *     guard $ if c then a == 1 else b == 1
          *     return (a, b, c)
          */
         AstModel model = newModel();
@@ -210,6 +372,39 @@ public class BoolArithmeticTest {
 
         ClaferSolver solver = ClaferCompiler.compile(model, Scope.defaultScope(1).intLow(-2).intHigh(2));
         assertEquals(10, solver.allInstances().length);
+    }
+
+    /**
+     * <pre>
+     * A -> integer
+     * B -> integer
+     * C ?
+     * [!(if some C then A.ref = 1 else B.ref = 2)]
+     * </pre>
+     */
+    @Test(timeout = 60000)
+    public void testNotIfThenElse() {
+        /*
+         * import Control.Monad
+         * 
+         * solutions = do
+         *     a <- [-2..2]
+         *     b <- [-2..2]
+         *     c <- [True, False]
+         *     guard $ not $ if c then a == 1 else b == 1
+         *     return (a, b, c)
+         */
+        AstModel model = newModel();
+
+        AstConcreteClafer a = model.addChild("A").withCard(1, 1).refTo(IntType);
+        AstConcreteClafer b = model.addChild("B").withCard(1, 1).refTo(IntType);
+        AstConcreteClafer c = model.addChild("C").withCard(0, 1);
+        model.addConstraint(not(ifThenElse(some(c),
+                equal(joinRef(global(a)), constant(1)),
+                equal(joinRef(global(b)), constant(2)))));
+
+        ClaferSolver solver = ClaferCompiler.compile(model, Scope.defaultScope(1).intLow(-2).intHigh(2));
+        assertEquals(40, solver.allInstances().length);
     }
 
     /**
@@ -245,6 +440,41 @@ public class BoolArithmeticTest {
 
         ClaferSolver solver = ClaferCompiler.compile(model, Scope.defaultScope(1).intLow(-2).intHigh(2));
         assertEquals(4, solver.allInstances().length);
+    }
+
+    /**
+     * <pre>
+     * A -> integer
+     * B -> integer
+     * C ?
+     * [!!(some C => (A.ref = 1 =&gt; B.ref = 2))]
+     * </pre>
+     */
+    @Test(timeout = 60000)
+    public void testNotNot() {
+        /*
+         * import Control.Monad
+         *
+         * solutions = do
+         *     a <- [-2..2]
+         *     b <- [-2..2]
+         *     c <- [True, False]
+         *     guard $ not $ not (c `implies` ((a == 1) `implies` (b == 2)))
+         *     return (a, b, c)
+         *     where
+         *         implies True False = False
+         *         implies _ _ = True
+         */
+        AstModel model = newModel();
+
+        AstConcreteClafer a = model.addChild("A").withCard(1, 1).refTo(IntType);
+        AstConcreteClafer b = model.addChild("B").withCard(1, 1).refTo(IntType);
+        AstConcreteClafer c = model.addChild("C").withCard(0, 1);
+        model.addConstraint(not(not(implies(some(c), implies(
+                equal(joinRef(global(a)), constant(1)), equal(joinRef(global(b)), constant(2)))))));
+
+        ClaferSolver solver = ClaferCompiler.compile(model, Scope.defaultScope(1).intLow(-2).intHigh(2));
+        assertEquals(46, solver.allInstances().length);
     }
 
     /**
