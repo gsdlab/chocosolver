d7535b27e717249c27e47fc490feaa849125c132
Jimmy Liang <paradoxical.reasoning@gmail.com>
2013-06-03 04:27:13 -0400

    Faster propagator for symmetry breaking.

--- src/main/java/org/clafer/choco/constraint/propagator/PropLexChainChannel.java
+++ src/main/java/org/clafer/choco/constraint/propagator/PropLexChainChannel.java
@@ -1,11 +1,13 @@
 package org.clafer.choco.constraint.propagator;
 
 import java.util.Arrays;
+import solver.Solver;
 import solver.constraints.propagators.Propagator;
 import solver.constraints.propagators.PropagatorPriority;
 import solver.exception.ContradictionException;
 import solver.variables.EventType;
 import solver.variables.IntVar;
+import solver.variables.VF;
 import util.ESat;
 
 /**
@@ -49,47 +51,91 @@ public class PropLexChainChannel extends Propagator<IntVar> {
         return EventType.INT_ALL_MASK();
     }
 
-    private static CharOrdering compareChar(IntVar a, IntVar b) {
-        if (a.instantiated() && b.instantiated() && a.getValue() == b.getValue()) {
-            return CharOrdering.EQ;
-        }
-        if (a.getLB() > b.getUB()) {
-            return CharOrdering.GT;
-        }
-        if (a.getUB() < b.getLB()) {
-            return CharOrdering.LT;
-        }
-        return CharOrdering.UNKNOWN;
+    private static Ordering compareString(IntVar[] a, IntVar[] b) {
+        return compareString(a, b, 0);
     }
 
-    private static CharOrdering compareString(IntVar[] a, IntVar[] b) throws ContradictionException {
-        for (int k = 0; k < a.length; k++) {
-            CharOrdering ord = compareChar(a[k], b[k]);
-            if (!CharOrdering.EQ.equals(ord)) {
-                return ord;
-            }
+    private static Ordering compareString(IntVar[] a, IntVar[] b, int index) {
+        if (index == a.length) {
+            return Ordering.EQ;
         }
-        // Only saw Ordering.EQ in the comparisons.
-        return CharOrdering.EQ;
+        Ordering ord = compare(a[index], b[index]);
+        switch (ord) {
+            case EQ:
+                return compareString(a, b, index + 1);
+            case LE:
+                switch (compareString(a, b, index + 1)) {
+                    case LT:
+                        return Ordering.LT;
+                    case LE:
+                    case EQ:
+                        return Ordering.LE;
+                    default:
+                        return Ordering.UNKNOWN;
+                }
+            case GE:
+                switch (compareString(a, b, index + 1)) {
+                    case GT:
+                        return Ordering.GT;
+                    case GE:
+                    case EQ:
+                        return Ordering.GE;
+                    default:
+                        return Ordering.UNKNOWN;
+                }
+            default:
+                return ord;
+        }
+    }
+
+    public static void main(String[] args) {
+        // string0[0] = -2 string0[1] = -2 string0[2] = -1 string0[3] = {-2,-1,0,1}
+        // [string3[0] = {-2,-1}, string3[1] = {-2,-1,0}, string3[2] = {-1,0}, string3[3] = 2
+        Solver solver = new Solver();
+        System.out.println(compareString(
+                new IntVar[]{VF.fixed(-2, solver), VF.fixed(-2, solver), VF.fixed(-3, solver), VF.enumerated("string03", -2, 1, solver)},
+                new IntVar[]{VF.enumerated("string30", -2, -1, solver), VF.enumerated("string31", -2, 0, solver), VF.enumerated("string32", -1, 0, solver), VF.fixed(2, solver)}));
     }
 
     private static Ordering compare(IntVar a, IntVar b) {
         if (a.instantiated() && b.instantiated() && a.getValue() == b.getValue()) {
             return Ordering.EQ;
         }
-        if (a.getLB() > b.getUB()) {
+        int aLb = a.getLB();
+        int aUb = a.getUB();
+        int bLb = b.getLB();
+        int bUb = b.getUB();
+        if (aLb > bUb) {
             return Ordering.GT;
         }
-        if (a.getLB() >= b.getUB()) {
+        if (aLb >= bUb) {
             return Ordering.GE;
         }
-        if (a.getUB() < b.getLB()) {
+        if (aUb < bLb) {
             return Ordering.LT;
         }
-        if (a.getUB() <= b.getLB()) {
+        if (aUb <= bLb) {
             return Ordering.LE;
         }
         return Ordering.UNKNOWN;
+    }
+
+    private boolean lessThanString(IntVar[] a, IntVar[] b) throws ContradictionException {
+        boolean changed = false;
+        for (int x = 0; x < a.length - 1; x++) {
+            switch (compare(a[x], b[x])) {
+                case LT:
+                case LE:
+                case UNKNOWN:
+                    return changed;
+                case GT:
+                case GE:
+                    changed |= equal(a[x], b[x]);
+                    break;
+            }
+        }
+        // All equal except for the last character.
+        return lessThan(a[a.length - 1], b[a.length - 1]) || changed;
     }
 
     private boolean lessThan(IntVar a, IntVar b) throws ContradictionException {
@@ -98,10 +144,39 @@ public class PropLexChainChannel extends Propagator<IntVar> {
                 | b.updateLowerBound(a.getLB() + 1, aCause);
     }
 
+    private boolean lessThanEqualString(IntVar[] a, IntVar[] b) throws ContradictionException {
+        for (int x = 0; x < a.length; x++) {
+            Ordering charOrd = compare(a[x], b[x]);
+            if (!Ordering.EQ.equals(charOrd)) {
+                return lessThanEqual(a[x], b[x]);
+            }
+        }
+        // The two strings are equal.
+        return false;
+    }
+
     private boolean lessThanEqual(IntVar a, IntVar b) throws ContradictionException {
         // Don't use short-circuit or
         return a.updateUpperBound(b.getUB(), aCause)
                 | b.updateLowerBound(a.getLB(), aCause);
+    }
+
+    private boolean greaterThanString(IntVar[] a, IntVar[] b) throws ContradictionException {
+        boolean changed = false;
+        for (int x = 0; x < a.length - 1; x++) {
+            switch (compare(a[x], b[x])) {
+                case GT:
+                case GE:
+                case UNKNOWN:
+                    return changed;
+                case LT:
+                case LE:
+                    changed |= equal(a[x], b[x]);
+                    break;
+            }
+        }
+        // All equal except for the last character.
+        return greaterThan(a[a.length - 1], b[a.length - 1]) || changed;
     }
 
     private boolean greaterThan(IntVar a, IntVar b) throws ContradictionException {
@@ -110,10 +185,29 @@ public class PropLexChainChannel extends Propagator<IntVar> {
                 | b.updateUpperBound(a.getUB() - 1, aCause);
     }
 
+    private boolean greaterThanEqualString(IntVar[] a, IntVar[] b) throws ContradictionException {
+        for (int x = 0; x < a.length; x++) {
+            Ordering charOrd = compare(a[x], b[x]);
+            if (!Ordering.EQ.equals(charOrd)) {
+                return greaterThanEqual(a[x], b[x]);
+            }
+        }
+        // The two strings are equal.
+        return false;
+    }
+
     private boolean greaterThanEqual(IntVar a, IntVar b) throws ContradictionException {
         // Don't use short-circuit or
         return a.updateLowerBound(b.getLB(), aCause)
                 | b.updateUpperBound(a.getUB(), aCause);
+    }
+
+    private boolean equalString(IntVar[] a, IntVar[] b) throws ContradictionException {
+        boolean changed = false;
+        for (int i = 0; i < a.length; i++) {
+            changed |= equal(a[i], b[i]);
+        }
+        return changed;
     }
 
     private boolean equal(IntVar a, IntVar b) throws ContradictionException {
@@ -124,100 +218,57 @@ public class PropLexChainChannel extends Propagator<IntVar> {
                 | b.updateUpperBound(a.getUB(), aCause);
     }
 
+    // Idempotent.
     private boolean propagateStrings() throws ContradictionException {
-        boolean repeat;
+        Update operations = new Update(strings.length);
+        int eqs = 0;
+        for (int i = 0; i < strings.length; i++) {
+            boolean equivalenceClass = false;
+            for (int j = i + 1; j < strings.length; j++) {
+                Ordering ord = compareString(strings[i], strings[j]);
+                switch (ord) {
+                    case EQ:
+                        equivalenceClass = true;
+                    // fallthrough
+                    case LT:
+                    case LE:
+                    case GT:
+                    case GE:
+                        operations.add(i, j, ord);
+                        break;
+                }
+            }
+            if (equivalenceClass) {
+                eqs++;
+            }
+        }
         boolean changed = false;
+        for (int i = 0; i < ints.length; i++) {
+            changed |= ints[i].updateUpperBound(ints.length - 1 - eqs, aCause);
+        }
+        boolean repeat;
         do {
-            repeat = false;
-            int eqs = 0;
-            for (int i = 0; i < strings.length; i++) {
-                boolean equivalenceClass = false;
-                for (int j = i + 1; j < strings.length; j++) {
-                    switch (compareString(strings[i], strings[j])) {
-                        case LT:
-                            repeat |= lessThan(ints[i], ints[j]);
-                            break;
-                        case GT:
-                            repeat |= greaterThan(ints[i], ints[j]);
-                            break;
-                        case EQ:
-                            repeat |= equal(ints[i], ints[j]);
-                            equivalenceClass = true;
-                            break;
-                    }
-                }
-                if (equivalenceClass) {
-                    eqs++;
-                }
-            }
-            for (int i = 0; i < ints.length; i++) {
-                repeat |= ints[i].updateUpperBound(ints.length - 1 - eqs, aCause);
-            }
+            repeat = operations.doIntUpdates();
             changed |= repeat;
         } while (repeat);
         return changed;
     }
 
+    // Idempotent.
     private boolean propagateInts() throws ContradictionException {
-        boolean repeat;
-        boolean changed = false;
-        do {
-            repeat = false;
-            for (int i = 0; i < ints.length; i++) {
-                for (int j = i + 1; j < ints.length; j++) {
-                    switch (compare(ints[i], ints[j])) {
-                        case LT:
-                            for (int x = 0; x < strings[i].length; x++) {
-                                CharOrdering ord = compareChar(strings[i][x], strings[j][x]);
-                                if (CharOrdering.UNKNOWN.equals(ord)) {
-                                    repeat |= lessThanEqual(strings[i][x], strings[j][x]);
-                                    break;
-                                }
-                                if (!CharOrdering.EQ.equals(ord)) {
-                                    repeat |= lessThan(strings[i][x], strings[j][x]);
-                                    break;
-                                }
-                            }
-                            break;
-                        case LE:
-                            for (int x = 0; x < strings[i].length; x++) {
-                                CharOrdering ord = compareChar(strings[i][x], strings[j][x]);
-                                if (!CharOrdering.EQ.equals(ord)) {
-                                    repeat |= lessThanEqual(strings[i][x], strings[j][x]);
-                                    break;
-                                }
-                            }
-                            break;
-                        case GT:
-                            for (int x = 0; x < strings[i].length; x++) {
-                                CharOrdering ord = compareChar(strings[i][x], strings[j][x]);
-                                if (CharOrdering.UNKNOWN.equals(ord)) {
-                                    repeat |= greaterThanEqual(strings[i][x], strings[j][x]);
-                                    break;
-                                }
-                                if (!CharOrdering.EQ.equals(ord)) {
-                                    repeat |= greaterThan(strings[i][x], strings[j][x]);
-                                    break;
-                                }
-                            }
-                            break;
-                        case GE:
-                            for (int x = 0; x < strings[i].length; x++) {
-                                CharOrdering ord = compareChar(strings[i][x], strings[j][x]);
-                                if (!CharOrdering.EQ.equals(ord)) {
-                                    repeat |= greaterThanEqual(strings[i][x], strings[j][x]);
-                                    break;
-                                }
-                            }
-                            break;
-                        case EQ:
-                            for (int y = 0; y < strings[i].length; y++) {
-                                repeat |= equal(strings[i][y], strings[j][y]);
-                            }
-                            break;
-                    }
+        Update operations = new Update(strings.length);
+        for (int i = 0; i < ints.length; i++) {
+            for (int j = i + 1; j < ints.length; j++) {
+                Ordering ord = compare(ints[i], ints[j]);
+                if (!Ordering.UNKNOWN.equals(ord)) {
+                    operations.add(i, j, ord);
                 }
             }
+        }
+        boolean changed = false;
+        boolean repeat;
+        do {
+            repeat = operations.doStringUpdates();
             changed |= repeat;
         } while (repeat);
         return changed;
@@ -225,9 +276,8 @@ public class PropLexChainChannel extends Propagator<IntVar> {
 
     @Override
     public void propagate(int evtmask) throws ContradictionException {
-        do {
-            propagateStrings();
-        } while (propagateInts());
+        propagateStrings();
+        while (propagateInts() && propagateStrings());
     }
 
     @Override
@@ -239,14 +289,14 @@ public class PropLexChainChannel extends Propagator<IntVar> {
     public ESat isEntailed() {
         for (int i = 0; i < strings.length; i++) {
             for (int j = i + 1; j < strings.length; j++) {
-                CharOrdering intOrdering = compareChar(ints[i], ints[j]);
-                CharOrdering ord = CharOrdering.EQ;
-                for (int k = 0; CharOrdering.EQ.equals(ord) && k < strings[i].length; k++) {
-                    ord = compareChar(strings[i][k], strings[j][k]);
+                Ordering intOrdering = compare(ints[i], ints[j]);
+                Ordering ord = Ordering.EQ;
+                for (int k = 0; Ordering.EQ.equals(ord) && k < strings[i].length; k++) {
+                    ord = compare(strings[i][k], strings[j][k]);
                 }
                 if (!intOrdering.equals(ord)
-                        && !CharOrdering.UNKNOWN.equals(intOrdering)
-                        && !CharOrdering.UNKNOWN.equals(ord)) {
+                        && !Ordering.UNKNOWN.equals(intOrdering)
+                        && !Ordering.UNKNOWN.equals(ord)) {
                     return ESat.FALSE;
                 }
             }
@@ -269,11 +319,82 @@ public class PropLexChainChannel extends Propagator<IntVar> {
         UNKNOWN;
     }
 
-    private static enum CharOrdering {
+    private class Update {
 
-        LT,
-        GT,
-        EQ,
-        UNKNOWN;
+        private int[] updates;
+        private int size = 0;
+
+        Update(int capacity) {
+            updates = new int[capacity * 3];
+        }
+
+        void add(int x, int y, Ordering ord) {
+            int offset = size * 3;
+            if (offset >= updates.length) {
+                updates = Arrays.copyOf(updates, updates.length * 2);
+            }
+            updates[offset] = x;
+            updates[offset + 1] = y;
+            updates[offset + 2] = ord.ordinal();
+            size++;
+        }
+
+        boolean doStringUpdates() throws ContradictionException {
+            boolean changed = false;
+            int cap = size * 3;
+            for (int i = 0; i < cap; i += 3) {
+                int x = updates[i];
+                int y = updates[i + 1];
+                switch (updates[i + 2]) {
+                    case 0: // LT
+                        changed |= lessThanString(strings[x], strings[y]);
+                        break;
+                    case 1: // LE
+                        changed |= lessThanEqualString(strings[x], strings[y]);
+                        break;
+                    case 2: // GT
+                        changed |= greaterThanString(strings[x], strings[y]);
+                        break;
+                    case 3: // GE
+                        changed |= greaterThanEqualString(strings[x], strings[y]);
+                        break;
+                    case 4: // EQ
+                        changed |= equalString(strings[x], strings[y]);
+                        break;
+                    default:
+                        throw new IllegalStateException();
+                }
+            }
+            return changed;
+        }
+
+        boolean doIntUpdates() throws ContradictionException {
+            boolean changed = false;
+            int cap = size * 3;
+            for (int i = 0; i < cap; i += 3) {
+                int x = updates[i];
+                int y = updates[i + 1];
+                switch (updates[i + 2]) {
+                    case 0: // LT
+                        changed |= lessThan(ints[x], ints[y]);
+                        break;
+                    case 1: // LE
+                        changed |= lessThanEqual(ints[x], ints[y]);
+                        break;
+                    case 2: // GT
+                        changed |= greaterThan(ints[x], ints[y]);
+                        break;
+                    case 3: // GE
+                        changed |= greaterThanEqual(ints[x], ints[y]);
+                        break;
+                    case 4: // EQ
+                        changed |= equal(ints[x], ints[y]);
+                        break;
+                    default:
+                        throw new IllegalStateException();
+                }
+            }
+            return changed;
+        }
     }
 }
