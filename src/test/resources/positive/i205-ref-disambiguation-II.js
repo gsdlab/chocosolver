fbd4d6a6a34a13f7d3b506baf6e7687364707a5e
Jimmy Liang <paradoxical.reasoning@gmail.com>
2013-10-19 02:46:45 -0400

    Add explicit refs to test case.

--- src/test/resources/positive/i205-ref-disambiguation-II.js
+++ src/test/resources/positive/i205-ref-disambiguation-II.js
@@ -5,4 +5,4 @@ c10_Car = Clafer("c10_Car").withCard(4, 4);
 c11_owner = c10_Car.addChild("c11_owner").withCard(1, 1);
 c21_Person = Clafer("c21_Person").withCard(4, 4);
 c11_owner.refToUnique(c21_Person);
-Constraint(all([disjDecl([c1 = local("c1"), c2 = local("c2")], global(c10_Car))], notEqual(join(c1, c11_owner), join(c2, c11_owner))));
+Constraint(all([disjDecl([c1 = local("c1"), c2 = local("c2")], global(c10_Car))], notEqual(joinRef(join(c1, c11_owner)), joinRef(join(c2, c11_owner)))));
