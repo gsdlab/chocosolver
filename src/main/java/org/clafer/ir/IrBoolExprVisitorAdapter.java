d4d38af16adf4ae4736196b6b0fd7bd06b5a50cc
Jimmy Liang <paradoxical.reasoning@gmail.com>
2013-10-20 17:55:59 -0400

    Adapter for mapping over boolean expressions.

---
+++ src/main/java/org/clafer/ir/IrBoolExprVisitorAdapter.java
@@ -0,0 +1,139 @@
+package org.clafer.ir;
+
+/**
+ * @param <A> the parameter type
+ * @param <B> the return type
+ * @author jimmy
+ */
+public abstract class IrBoolExprVisitorAdapter<A, B> implements IrBoolExprVisitor<A, B> {
+
+    @Override
+    public B visit(IrBoolVar ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrNot ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrAnd ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrLone ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrOne ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrOr ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrImplies ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrNotImplies ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrIfThenElse ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrIfOnlyIf ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrXor ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrWithin ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrNotWithin ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrCompare ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrSetTest ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrMember ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrNotMember ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrSubsetEq ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrBoolChannel ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrIntChannel ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrSortStrings ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrSortSets ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrSortStringsChannel ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrAllDifferent ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrSelectN ir, A a) {
+        return null;
+    }
+
+    @Override
+    public B visit(IrFilterString ir, A a) {
+        return null;
+    }
+}
