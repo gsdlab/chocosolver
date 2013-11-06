59f4ad1c4a14fd5f626c4c6052bfc1d8e2af2bc0
Jimmy Liang <paradoxical.reasoning@gmail.com>
2013-11-06 16:43:16 -0500

    Stronger duplicate detection.

--- src/main/java/org/clafer/ir/analysis/DuplicateConstraints.java
+++ src/main/java/org/clafer/ir/analysis/DuplicateConstraints.java
@@ -23,6 +23,20 @@ public class DuplicateConstraints {
     private DuplicateConstraints() {
     }
 
+    /**
+     * Reduce.
+     *
+     * <ol>
+     * <li>a = b & a ≤ b to a = b</li>
+     * <li>a ≠ b & a &lt; b to a &lt; b</li>
+     * <li>a ≠ b & a ≤ b to a &lt; b</li>
+     * <li>a &lt; b & a ≤ b to a &lt; b</li>
+     * <li>a ≤ b & b ≤ a to a = b</li>
+     * </ol>
+     *
+     * @param module
+     * @return
+     */
     public static IrModule removeDuplicates(IrModule module) {
         HashSet<Pair<IrIntExpr, IrIntExpr>> equal = new HashSet<>();
         HashSet<Pair<IrIntExpr, IrIntExpr>> notEqual = new HashSet<>();
@@ -38,26 +52,41 @@ public class DuplicateConstraints {
                 IrIntExpr right = compare.getRight();
                 Pair<IrIntExpr, IrIntExpr> pair = new Pair<>(left, right);
                 Pair<IrIntExpr, IrIntExpr> converse = new Pair<>(right, left);
+
                 switch (compare.getOp()) {
                     case Equal:
                         lessThanEqual.remove(pair);
                         lessThanEqual.remove(converse);
                         equal.add(pair);
+                        equal.remove(converse);
                         break;
                     case NotEqual:
-                        if (!lessThan.contains(pair)
+                        if (lessThanEqual.remove(pair)) {
+                            lessThan.add(pair);
+                        } else if (lessThanEqual.remove(converse)) {
+                            lessThan.add(converse);
+                        } else if (!lessThan.contains(pair)
                                 && !lessThan.contains(converse)) {
                             notEqual.add(pair);
+                            notEqual.remove(converse);
                         }
                         break;
                     case LessThan:
                         notEqual.remove(pair);
                         notEqual.remove(converse);
+                        lessThanEqual.remove(pair);
                         lessThan.add(pair);
                         break;
                     case LessThanEqual:
-                        if (!equal.contains(pair)
-                                && !equal.contains(converse)) {
+                        if (notEqual.remove(pair)) {
+                            lessThan.add(pair);
+                        } else if (notEqual.remove(converse)) {
+                            lessThan.add(converse);
+                        } else if (lessThanEqual.remove(converse)) {
+                            equal.add(pair);
+                        } else if (!equal.contains(pair)
+                                && !equal.contains(converse)
+                                && !lessThan.contains(pair)) {
                             lessThanEqual.add(pair);
                         }
                         break;
