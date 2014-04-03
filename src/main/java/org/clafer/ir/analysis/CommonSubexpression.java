062fa99e2bf432a60da414a952f52e0d57ddb38e
JLiangWaterloo <paradoxical.reasoning@gmail.com>
2014-04-02 21:09:46 -0400

    Minor refactor.

--- src/main/java/org/clafer/ir/analysis/CommonSubexpression.java
+++ src/main/java/org/clafer/ir/analysis/CommonSubexpression.java
@@ -3,12 +3,14 @@ package org.clafer.ir.analysis;
 import java.util.HashSet;
 import java.util.Set;
 import org.clafer.ir.IrArrayToSet;
+import org.clafer.ir.IrCard;
 import org.clafer.ir.IrElement;
 import org.clafer.ir.IrExpr;
 import org.clafer.ir.IrJoinFunction;
 import org.clafer.ir.IrJoinRelation;
 import org.clafer.ir.IrModule;
 import org.clafer.ir.IrTraverser;
+import org.clafer.ir.IrVar;
 
 /**
  *
@@ -30,35 +32,39 @@ public class CommonSubexpression {
         private final Set<IrExpr> seen = new HashSet<>();
         private final Set<IrExpr> duplicates = new HashSet<>();
 
+        private void check(IrExpr expr) {
+            if (!(expr instanceof IrVar) && !seen.add(expr)) {
+                duplicates.add(expr);
+            }
+        }
+
+        @Override
+        public Void visit(IrCard ir, Void a) {
+            check(ir.getSet());
+            return super.visit(ir, a);
+        }
+
         @Override
         public Void visit(IrElement ir, Void a) {
-            if (!seen.add(ir)) {
-                duplicates.add(ir);
-            }
+            check(ir);
             return super.visit(ir, a);
         }
 
         @Override
         public Void visit(IrArrayToSet ir, Void a) {
-            if (!seen.add(ir)) {
-                duplicates.add(ir);
-            }
+            check(ir);
             return super.visit(ir, a);
         }
 
         @Override
         public Void visit(IrJoinRelation ir, Void a) {
-            if (!seen.add(ir)) {
-                duplicates.add(ir);
-            }
+            check(ir);
             return super.visit(ir, a);
         }
 
         @Override
         public Void visit(IrJoinFunction ir, Void a) {
-            if (!seen.add(ir)) {
-                duplicates.add(ir);
-            }
+            check(ir);
             return super.visit(ir, a);
         }
     }
