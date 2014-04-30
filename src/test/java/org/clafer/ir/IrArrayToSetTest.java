5cf92e4500c45f5d91a4b14cb4e3d167af3b7baf
JLiangWaterloo <paradoxical.reasoning@gmail.com>
2014-04-30 14:47:41 -0400

    Array to set test.

---
+++ src/test/java/org/clafer/ir/IrArrayToSetTest.java
@@ -0,0 +1,29 @@
+package org.clafer.ir;
+
+import org.clafer.choco.constraint.Constraints;
+import org.clafer.ir.IrQuickTest.Solution;
+import static org.clafer.ir.Irs.*;
+import org.clafer.test.NonEmpty;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import solver.constraints.Constraint;
+import solver.variables.CSetVar;
+import solver.variables.IntVar;
+
+/**
+ *
+ * @author jimmy
+ */
+@RunWith(IrQuickTest.class)
+public class IrArrayToSetTest {
+
+    @Test(timeout = 60000)
+    public IrBoolExpr setup(@NonEmpty IrIntVar[] array, int globalCardinality, IrSetVar set) {
+        return equal(arrayToSet(array, globalCardinality > 0 ? globalCardinality : null), set);
+    }
+
+    @Solution
+    public Constraint setup(IntVar[] array, int globalCardinality, CSetVar set) {
+        return Constraints.arrayToSet(array, set.getSet(), set.getCard(), globalCardinality > 0 ? globalCardinality : null);
+    }
+}
