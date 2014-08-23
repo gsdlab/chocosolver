0c011b8958eb13f0f9b3f7f4574ea596cc00ca0a
JLiangWaterloo <paradoxical.reasoning@gmail.com>
2014-08-23 18:18:52 -0400

    Add new relational classes.

---
+++ src/main/java/org/clafer/ast/AstChildRelation.java
@@ -0,0 +1,25 @@
+package org.clafer.ast;
+
+import org.clafer.common.Check;
+
+/**
+ *
+ * @author jimmy
+ */
+public class AstChildRelation implements AstSetExpr {
+
+    private final AstConcreteClafer childRelation;
+
+    AstChildRelation(AstConcreteClafer childRelation) {
+        this.childRelation = Check.notNull(childRelation);
+    }
+
+    public AstConcreteClafer getChildRelation() {
+        return childRelation;
+    }
+
+    @Override
+    public <A, B> B accept(AstExprVisitor<A, B> visitor, A a) {
+        return visitor.visit(this, a);
+    }
+}
