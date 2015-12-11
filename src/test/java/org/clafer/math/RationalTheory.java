e18f54be6b91ff2098bd76f4e2a463b1ecc24178
JLiangWaterloo <paradoxical.reasoning@gmail.com>
2015-12-11 15:29:07 -0500

    Unit tests for Rational.

---
+++ src/test/java/org/clafer/math/RationalTheory.java
@@ -0,0 +1,26 @@
+package org.clafer.math;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author jimmy
+ */
+@RunWith(Theories.class)
+public class RationalTheory {
+
+    @DataPoints
+    public static final Rational[] rationals = new Rational[]{
+        new Rational(-1), new Rational(0), new Rational(1),
+        new Rational(1, 2), new Rational(3, 2), new Rational(4, 3)
+    };
+
+    @Theory
+    public void testCeilGreaterEqualFloor(Rational r) {
+        assertTrue(r.ceil() >= r.floor());
+    }
+}
