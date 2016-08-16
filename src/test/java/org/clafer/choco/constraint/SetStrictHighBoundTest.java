e516cdfcb41b8c537115dd94ad7d0db29166a301
JLiangWaterloo <paradoxical.reasoning@gmail.com>
2016-08-16 17:45:56 -0400

    Add no card to the set variables of set strict high bound test.

--- src/test/java/org/clafer/choco/constraint/SetStrictHighBoundTest.java
+++ src/test/java/org/clafer/choco/constraint/SetStrictHighBoundTest.java
@@ -11,6 +11,7 @@ import org.chocosolver.solver.constraints.Constraint;
 import org.chocosolver.solver.variables.IntVar;
 import org.chocosolver.solver.variables.SetVar;
 import static org.chocosolver.solver.variables.Var.*;
+import org.clafer.test.NoCard;
 
 /**
  *
@@ -45,7 +46,7 @@ public class SetStrictHighBoundTest {
 
     @ArcConsistent
     @Test(timeout = 60000)
-    public Constraint setup(SetVar set, IntVar bound) {
+    public Constraint setup(@NoCard SetVar set, IntVar bound) {
         return Constraints.stritctHighBound(set, bound);
     }
 }
