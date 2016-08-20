780901a14509e6f953deb6afae8fc7dd9a31a059
JLiangWaterloo <paradoxical.reasoning@gmail.com>
2016-08-20 08:32:22 -0400

    Mark int channel constraint as arc consistent.

--- src/test/java/org/clafer/choco/constraint/IntChannelTest.java
+++ src/test/java/org/clafer/choco/constraint/IntChannelTest.java
@@ -11,6 +11,7 @@ import org.chocosolver.solver.constraints.Constraint;
 import org.chocosolver.solver.variables.IntVar;
 import org.chocosolver.solver.variables.SetVar;
 import static org.chocosolver.solver.variables.Var.*;
+import org.clafer.test.NoCard;
 
 /**
  *
@@ -39,8 +40,9 @@ public class IntChannelTest {
         }
     }
 
+    @ArcConsistent
     @Test(timeout = 60000)
-    public Constraint setup(@NonEmpty SetVar[] sets, @NonEmpty IntVar[] ints) {
+    public Constraint setup(@NoCard @NonEmpty SetVar[] sets, @NonEmpty IntVar[] ints) {
         return Constraints.intChannel(sets, ints);
     }
 }
