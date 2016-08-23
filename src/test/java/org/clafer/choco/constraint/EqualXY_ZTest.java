05c977fe5d4391a8e6a380a2a7ba92a77e7219f7
JLiangWaterloo <paradoxical.reasoning@gmail.com>
2016-08-23 10:24:33 -0400

    Fix x + y != z constraint pruning valid solutions.

--- src/test/java/org/clafer/choco/constraint/EqualXY_ZTest.java
+++ src/test/java/org/clafer/choco/constraint/EqualXY_ZTest.java
@@ -1,8 +1,11 @@
 package org.clafer.choco.constraint;
 
+import org.chocosolver.solver.Model;
 import org.chocosolver.solver.constraints.Constraint;
 import org.chocosolver.solver.variables.IntVar;
+import static org.clafer.choco.constraint.ConstraintQuickTest.$;
 import org.clafer.choco.constraint.ConstraintQuickTest.Check;
+import org.clafer.choco.constraint.ConstraintQuickTest.Input;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +16,14 @@ import org.junit.runner.RunWith;
  */
 @RunWith(ConstraintQuickTest.class)
 public class EqualXY_ZTest {
+
+    @Input(solutions = 7)
+    public Object testEqualXY_Z(Model model) {
+        return $(model.intVar("x", -1, 1),
+                model.intVar("y", -1, 1),
+                model.intVar("z", -1, 1)
+        );
+    }
 
     @Check
     public void check(int x, int y, int sum) {
