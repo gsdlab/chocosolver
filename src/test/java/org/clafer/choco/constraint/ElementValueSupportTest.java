daf4e93abf60c4f7f6126a13ce467b9d71abef22
JLiangWaterloo <paradoxical.reasoning@gmail.com>
2016-08-23 16:26:06 -0400

    Stronger element value support constraint entailment check.

--- src/test/java/org/clafer/choco/constraint/ElementValueSupportTest.java
+++ src/test/java/org/clafer/choco/constraint/ElementValueSupportTest.java
@@ -1,8 +1,11 @@
 package org.clafer.choco.constraint;
 
+import org.chocosolver.solver.Model;
 import org.chocosolver.solver.constraints.Constraint;
 import org.chocosolver.solver.variables.IntVar;
+import static org.clafer.choco.constraint.ConstraintQuickTest.$;
 import org.clafer.choco.constraint.ConstraintQuickTest.Check;
+import org.clafer.choco.constraint.ConstraintQuickTest.Input;
 import org.clafer.choco.constraint.propagator.PropElementValueSupport;
 import org.clafer.test.NonEmpty;
 import static org.junit.Assert.assertTrue;
@@ -15,6 +18,12 @@ import org.junit.runner.RunWith;
  */
 @RunWith(ConstraintQuickTest.class)
 public class ElementValueSupportTest {
+
+    @Input(solutions = 224)
+    public Object testElementValueSupport(Model model) {
+        return $(model.intVar("value", 0, 3), model.intVarArray("array", 3, 0, 3),
+                model.intVar("index", -1, 0), 1, 0);
+    }
 
     @Check
     public void check(int value, int[] array, int index, int offset, int support) {
