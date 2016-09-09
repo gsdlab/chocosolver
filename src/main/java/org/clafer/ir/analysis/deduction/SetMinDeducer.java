7b82464b201bc86cb220ea7b4079e62c0b1c80db
JLiangWaterloo <paradoxical.reasoning@gmail.com>
2016-09-09 11:07:16 -0400

    Improve set min deduction.

--- src/main/java/org/clafer/ir/analysis/deduction/SetMinDeducer.java
+++ src/main/java/org/clafer/ir/analysis/deduction/SetMinDeducer.java
@@ -1,6 +1,7 @@
 package org.clafer.ir.analysis.deduction;
 
 import org.clafer.domain.Domain;
+import org.clafer.domain.Domains;
 import org.clafer.ir.IrSetExpr;
 import org.clafer.ir.IrSetMin;
 
@@ -17,10 +18,24 @@ class SetMinDeducer implements IntDeducer<IrSetMin> {
         boolean nonEmpty = set.getCard().getLowBound() > 0
                 || !domain.contains(ir.getDefaultValue());
 
-        if (nonEmpty && !set.getKer().intersects(domain)) {
-            deduction.cardGreaterThanEqual(set, set.getKer().size() + 1);
-            if (set.getKer().size() + 1 == set.getCard().getHighBound()) {
-                deduction.envSubsetOf(set, set.getEnv().intersection(domain).union(set.getKer()));
+        if (nonEmpty) {
+            if (!set.getKer().intersects(domain)) {
+                // Set needs another element.
+                deduction.cardGreaterThanEqual(set, set.getKer().size() + 1);
+                if (set.getKer().size() + 1 == set.getCard().getHighBound()) {
+                    // Set can take only one more element.
+                    deduction.envSubsetOf(set, set.getEnv().intersection(domain).union(set.getKer()));
+                }
+            }
+            int min = domain.getLowBound();
+            if (!set.getKer().isEmpty()) {
+                min = Math.min(min, set.getKer().getLowBound());
+            }
+            if (set.getEnv().getLowBound() < min) {
+                deduction.envRemove(set, Domains.boundDomain(set.getEnv().getLowBound(), min - 1));
+            }
+            if (domain.isConstant()) {
+                deduction.kerContains(set, domain.getLowBound());
             }
         }
     }
