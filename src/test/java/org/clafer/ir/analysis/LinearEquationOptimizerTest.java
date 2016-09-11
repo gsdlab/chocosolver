98760abd6a6a840c79fe116c09e1e0db85aa81a9
JLiangWaterloo <paradoxical.reasoning@gmail.com>
2016-09-11 14:40:10 -0400

    Test linear optimizer optimizes sum on sorted operands.

--- src/test/java/org/clafer/ir/analysis/LinearEquationOptimizerTest.java
+++ src/test/java/org/clafer/ir/analysis/LinearEquationOptimizerTest.java
@@ -1,9 +1,11 @@
 package org.clafer.ir.analysis;
 
+import org.clafer.domain.Domains;
 import org.clafer.ir.IrIntVar;
 import org.clafer.ir.IrModule;
 import org.clafer.ir.Irs;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
@@ -35,5 +37,23 @@ public class LinearEquationOptimizerTest {
         module = LinearEquationOptimizer.optimize(module);
 
         assertEquals(1, module.getConstraints().size());
+    }
+
+    @Test
+    public void testCombineInequalties() {
+        IrModule module = new IrModule();
+        IrIntVar i0 = Irs.boundInt("i0", 0, 4);
+        IrIntVar i1 = Irs.boundInt("i1", 0, 4);
+        IrIntVar i2 = Irs.boundInt("i2", 0, 4);
+        IrIntVar i3 = Irs.boundInt("i3", 0, 4);
+        module.addConstraint(Irs.greaterThan(Irs.add(i0, i1, i2, i3), 0));
+        module.addConstraint(Irs.greaterThanEqual(i0, i1));
+        module.addConstraint(Irs.greaterThanEqual(i1, i2));
+        module.addConstraint(Irs.greaterThanEqual(i2, i3));
+
+        module = LinearEquationOptimizer.optimize(module);
+
+        assertTrue(module.getConstraints().contains(
+                Irs.lessThanEqual(Irs.One, Irs.mul(4, i0, Domains.boundDomain(0, 16)))));
     }
 }
